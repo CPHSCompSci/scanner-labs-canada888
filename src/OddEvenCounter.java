da6a77921cded3db98c9ff8ff4a60034a276b52e
cheri <cheri@10.19.18.113>
2019-10-22 11:34:29 -0600

    Final OddEvenCounter

--- src/OddEvenCounter.java
+++ src/OddEvenCounter.java
@@ -11,30 +11,54 @@ public class OddEvenCounter
 
    public OddEvenCounter()
    {
+	   setLine("");
    }
 
    public OddEvenCounter(String s)
    {
+	   setLine(s);
    }
 
 	public void setLine(String s)
 	{
+		this.line = s;
 	}
 
 	public int getEvenCount()
 	{
-		int count= 0;
+		int count=0;
+		Scanner input = new Scanner(line);
+		
+		while(input.hasNextInt())
+		{
+			if(input.nextInt() % 2 == 0)
+			{
+				//input.nextInt();
+				count++;
+			}
+			
+		}
 		return count;
 	}
 
 	public int getOddCount()
 	{
-		int count= 0;
+		int count=0;
+		Scanner hello = new Scanner(line);
+		
+		while(hello.hasNextInt())
+		{
+			if(hello.nextInt() % 2 == 1)
+			{
+				//hello.nextInt();
+				count++;
+			}
+		}
 		return count;
 	}
 
-	public String toString( )
+	public String toString()
 	{
-		return "";
+		return "Even count = " + getEvenCount() + "\n" + "Odd count = " + getOddCount();
 	}
 }
--- src/OddEvenCounterRunner.java
+++ src/OddEvenCounterRunner.java
@@ -1,13 +1,20 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import java.util.Scanner;
-import static java.lang.System.*;
-
+//import java.lang.System.*;  
+import java.io.File;
+import java.io.FileNotFoundException;
 public class OddEvenCounterRunner
 {
-   public static void main(String args[])
+   public static void main( String args[]) throws FileNotFoundException
    {
+	   Scanner scan = new Scanner(new File("OELine.txt"));
+	   
+	   for(;scan.hasNextLine();)
+	   {
+		   String line = scan.nextLine();
+		   OddEvenCounter lc = new OddEvenCounter(line);
+		   
+		   System.out.println(line);
+		   System.out.println(lc.toString());
+	   }
 	}
 }
